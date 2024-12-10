406d1174c4eaedb48acb48fd60c4935f5e9646a0
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2024-12-09 23:48:52 -0500

    day8

---
+++ day8/sol1.py
@@ -0,0 +1,33 @@
+from fractions import Fraction
+locations = []
+
+with open('input.txt', 'r') as file:
+    i = 0
+    for line in file:
+        line = list(line.strip())
+        for j in range(len(line)):
+            locations.append((i, j, line[j]))
+        i += 1
+
+def slope(x1, y1, x2, y2):
+    if x2 == x1:  # Vertical line
+        return float('inf')
+    return Fraction(y2 - y1, x2 - x1)  # Exact slope as a fraction
+
+def distance_squared(x1, y1, x2, y2):
+    return (x2 - x1) ** 2 + (y2 - y1) ** 2
+
+antinode_set = set()
+for antenna1 in locations:
+    for antenna2 in locations:
+        if antenna1[2] == antenna2[2] and antenna1[2] != '.' and antenna1 != antenna2:
+            m = slope(antenna1[0], antenna1[1], antenna2[0], antenna2[1])
+            d = distance_squared(antenna1[0], antenna1[1], antenna2[0], antenna2[1])
+
+            for antinode in locations:
+                if antinode != antenna1 and antinode != antenna2 and m == slope(antenna1[0], antenna1[1], antinode[0], antinode[1]) and \
+                (d == distance_squared(antenna1[0], antenna1[1], antinode[0], antinode[1]) or d == distance_squared(antenna2[0], antenna2[1], antinode[0], antinode[1])):
+                    antinode_set.add((antinode[0], antinode[1]))
+        
+
+print(len(antinode_set))
